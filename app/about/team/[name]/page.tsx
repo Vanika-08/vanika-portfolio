0cde403ea281fb71231a387674f77b69f4f83bb9
Vanika-08 <vanikajain213@gmail.com>
2026-04-23 15:02:05 +0530

    fix typescript error

--- app/about/team/[name]/page.tsx
+++ app/about/team/[name]/page.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+type Props = {
+  params: {
+    name: string;
+  };
+};
 
-const TeamDetailPage = async ({ params }) => {
-    const name = (await params).name;
-  return <div className="text-6xl text-red-950">TeamDetailPage {name}</div>;
+const TeamDetailPage = async ({ params }: Props) => {
+  const name = params.name;
+
+  return (
+    <div className="text-6xl text-red-950">
+      TeamDetailPage {name}
+    </div>
+  );
 };
 
 export default TeamDetailPage;
